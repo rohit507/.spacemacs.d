9681f74df8b56d48ff7f6041ece261e3af10fb02
Rohit Ramesh <Rohit507@gmail.com>
2018-09-06 16:36:18 -0700

    split initialization into two separate files

--- shared-init.el
+++ shared-init.el
@@ -8,13 +8,38 @@
 ;; All configuration is housed in personal layers - see README.
 ;; `init.el' configures spacemacs, defining required `dotspacemacs/...' functions.
 
-;; Machine specific config goes here 
+
+(defvar linux? (eq system-type 'gnu/linux)
+  "Are we on a gnu/linux machine?")
+
+(defun os-path (path)
+  "Prepend drive label to PATH if on windows machine."
+  (if linux?
+      path
+    (expand-file-name path "c:")))
 
 ;; dotspacemacs/init and all the default configuration variables are stored in
 ;; this file. This is where you change things like fonts, display settings, and
 ;; the like
-(load "~/.spacemacs.d/init/shared-init")
+(load "~/.spacemacs.d/init/config")
 
+;; dotspacemacs/layers declarations are all in this file
+(load "~/.spacemacs.d/init/layers")
+
+;; dotspacemacs/user-config is defined here
+(load "~/.spacemacs.d/init/user-config")
+
+
+(defun dotspacemacs/user-init ()
+  "Initialization function for user code.
+   It is called immediately after `dotspacemacs/init', before layer configuration
+   executes.
+   This function is mostly useful for variables that need to be set
+   before packages are loaded. If you are unsure, you should try in setting them in
+   `dotspacemacs/user-config' first.
+   Package independent settings to run before `dotspacemacs/user-config'."
+
+  )
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
